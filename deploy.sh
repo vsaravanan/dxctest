1534a6ef87c849ef3f95e0775dbe37103d3e3434
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-06-08 20:28:28 +0800

    pm2 restart dxctest

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 
 git pull
-
 # -Dmaven.test.skip=true
 mvn clean package install  -T 1C
-exit
-#pm2 restart dxctest
-#sleep 10
-#tail -f /data/logs/dxctest/*
+
+pm2 restart dxctest
+sleep 10
+tail -f /data/logs/dxctest/*
