2cccfb6184d96ba271c533a0fd23db9ec952ff12
Viswanathan Saravanan <saravanan.resume@gmail.com>
2026-06-08 17:13:52 +0800

    compiling: error: release version 26 not supported

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 
-git pull
+# git pull
 mvn clean package install -Dmaven.test.skip=true -T 1C
-pm2 restart dxctest
-sleep 10
-tail -f /data/logs/dxctest/*
+exit
+#pm2 restart dxctest
+#sleep 10
+#tail -f /data/logs/dxctest/*
